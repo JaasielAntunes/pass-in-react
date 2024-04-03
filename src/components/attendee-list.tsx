057826107a38ba3af975c5351450acbb3054303c
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-02 22:22:16 -0300

    Espaçamento lateral no attendee-list

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -9,7 +9,7 @@ import {
 
 export function AttendeeList() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 px-4">
       <div className="flex gap-3 items-center">
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div
